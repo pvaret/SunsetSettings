54afc5ab75042cccffe75b8eb97fa7a31a622919
P. Varet <p.varet@gmail.com>
2025-06-14 23:06:15 +0100

    Removed pytest-case dependency.
    Attractive API, but doesn't support decorating methods, which we use lots of.
    
    Some minor pyproject.toml cleanup.

--- tests/test_stringutils.py
+++ tests/test_stringutils.py
@@ -1,19 +1,25 @@
 # pyright: reportMissingTypeStubs=false
 
 from typing import Any
-from pytest_case import case
+
+import pytest
 
 from sunset.stringutils import collate_by_prefix, split_on
 
 
-@case("empty input", string="", expected=("", ""))
-@case("no split point", string="a", expected=("a", ""))
-@case("one split point", string="a.b", expected=("a", "b"))
-@case("two split points", string="a.b.c", expected=("a", "b.c"))
-@case("split point before", string=".a", expected=("", "a"))
-@case("two split points before", string="..a", expected=("", ".a"))
-@case("split point after", string="a.", expected=("a", ""))
-@case("split points before and after", string=".a.", expected=("", "a."))
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        ("", ("", "")),
+        ("a", ("a", "")),
+        ("a.b", ("a", "b")),
+        ("a.b.c", ("a", "b.c")),
+        (".a", ("", "a")),
+        ("..a", ("", ".a")),
+        ("a.", ("a", "")),
+        (".a.", ("", "a.")),
+    ],
+)
 def test_split_on(string: str, expected: tuple[str, str]) -> None:
     assert split_on(".")(string) == expected
 
@@ -21,79 +27,39 @@ def test_split_on(string: str, expected: tuple[str, str]) -> None:
 _SENTINEL = "test"
 
 
-@case("empty inputs", inputs=[], expected={})
-@case(
-    "empty prefixes",
-    inputs=[
-        ("", _SENTINEL),
-        ("", _SENTINEL),
+@pytest.mark.parametrize(
+    "inputs, expected",
+    [
+        ([], {}),
+        ([("", _SENTINEL), ("", _SENTINEL)], {"": [("", _SENTINEL), ("", _SENTINEL)]}),
+        (
+            [("a", _SENTINEL), ("b", _SENTINEL)],
+            {"a": [("", _SENTINEL)], "b": [("", _SENTINEL)]},
+        ),
+        (
+            [("a.b", _SENTINEL), ("a.c", _SENTINEL)],
+            {"a": [("b", _SENTINEL), ("c", _SENTINEL)]},
+        ),
+        (
+            [("a.b.c", _SENTINEL), ("a.b.d", _SENTINEL), ("x.b.c", _SENTINEL)],
+            {
+                "a": [("b.c", _SENTINEL), ("b.d", _SENTINEL)],
+                "x": [("b.c", _SENTINEL)],
+            },
+        ),
+        (
+            [
+                ("a", _SENTINEL),
+                ("a.b", _SENTINEL),
+                ("a.b.c", _SENTINEL),
+                ("b.c", _SENTINEL),
+            ],
+            {
+                "a": [("", _SENTINEL), ("b", _SENTINEL), ("b.c", _SENTINEL)],
+                "b": [("c", _SENTINEL)],
+            },
+        ),
     ],
-    expected={
-        "": [
-            ("", _SENTINEL),
-            ("", _SENTINEL),
-        ],
-    },
-)
-@case(
-    "no split point",
-    inputs=[
-        ("a", _SENTINEL),
-        ("b", _SENTINEL),
-    ],
-    expected={
-        "a": [("", _SENTINEL)],
-        "b": [("", _SENTINEL)],
-    },
-)
-@case(
-    "one split point",
-    inputs=[
-        ("a.b", _SENTINEL),
-        ("a.c", _SENTINEL),
-    ],
-    expected={
-        "a": [
-            ("b", _SENTINEL),
-            ("c", _SENTINEL),
-        ],
-    },
-)
-@case(
-    "two split points",
-    inputs=[
-        ("a.b.c", _SENTINEL),
-        ("a.b.d", _SENTINEL),
-        ("x.b.c", _SENTINEL),
-    ],
-    expected={
-        "a": [
-            ("b.c", _SENTINEL),
-            ("b.d", _SENTINEL),
-        ],
-        "x": [
-            ("b.c", _SENTINEL),
-        ],
-    },
-)
-@case(
-    "variable split points",
-    inputs=[
-        ("a", _SENTINEL),
-        ("a.b", _SENTINEL),
-        ("a.b.c", _SENTINEL),
-        ("b.c", _SENTINEL),
-    ],
-    expected={
-        "a": [
-            ("", _SENTINEL),
-            ("b", _SENTINEL),
-            ("b.c", _SENTINEL),
-        ],
-        "b": [
-            ("c", _SENTINEL),
-        ],
-    },
 )
 def test_collate_by_prefix(
     inputs: list[tuple[str, Any]], expected: dict[str, list[tuple[str, Any]]]
